7a3407c73d32d7f4e32c4ad5ddfb91a4281a2acc
xTarzx <telmomartins2000@gmail.com>
2025-10-21 15:07:54 +0100

    idk

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -45,7 +45,7 @@ async fn main() -> anyhow::Result<()> {
         };
 
         let key = match key_event.key.as_str() {
-            "KeyA" => Some(Key::A),
+            "KeySpace" => Some(Key::Space),
             _ => None,
         };
 
